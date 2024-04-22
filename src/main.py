60ae609511f743e8c2ec1f6e93d01a7775a455e2
Rachit Mukkamala <rsmukk@mit.edu>
2024-04-22 12:17:10 -0400

    updated gen script to add TR IDs

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from tqdm import tqdm
 
 def process_csv(csv_file):
     
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        left_scaffolds = []
-        for row in reader:
-            sequence = ''.join(row)
-            scaffold_left = sequence.replace(".", "")
-            left_scaffolds.append(scaffold_left)
+    seq_table = pd.read_csv(csv_file)
+    left_scaffolds = {}
+    for row in range(seq_table.shape[0]):
+        id = seq_table.iloc[row, 0]
+        sequence = ''.join(seq_table.iloc[row, 1])
+        scaffold_left = sequence.replace(".", "")
+        left_scaffolds[id] = scaffold_left
             
     return left_scaffolds
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         right_scaffold = file.read().strip()
     model_type = "oa_dm_38M"
 
-    for i in tqdm(range(len(left_scaffolds))):
+    for id in tqdm(left_scaffolds.keys()):
         for cdr3_len in tqdm(range(8,12)): # first 3 residues already in prompt
             for j in range(num_repeats):
                 command = [
@@ -33,8 +33,9 @@ if __name__ == "__main__":
                     f"--model-type={model_type}",
                     "--gpus=6",
                     "--cond-task=tcr",
-                    f"--scaffold_chain_left={left_scaffolds[i]}",
+                    f"--scaffold_chain_left={left_scaffolds[id]}",
                     f"--scaffold_chain_right={right_scaffold}",
                     f"--cdr3_len={cdr3_len}"
                 ]
                 subprocess.run(command)
+
