7b79f0cddbbe75f04541ba741e40a2ff528fb6b0
Bridget Li <42754047+bridgetli@users.noreply.github.com>
2024-05-11 17:24:23 -0400

    Update kl_divergence.py
    added hamming distance calculation for sequence diversity

--- src/kl_divergence.py
+++ src/kl_divergence.py
@@ -55,6 +55,37 @@ def calc_unconditiona_kl(gen_file):
     kl_value = kl(true_seqs, gen_seqs, kl_loss)
     print(f"KL Divergence = {kl_value}")
 
+def calc_diversity(gen_file):
+    with open('data/trbc2_seq.txt', 'r') as file:
+        const_region = file.read().strip()
+
+    df = pd.read_csv(gen_file, header=None)
+    full_seqs = df[0].tolist()
+    cdr_lens = df[1].astype(int).tolist()
+    const_idxs = [x.find(const_region) for x in full_seqs]
+    gen_seqs = [full_seqs[i][const_idxs[i] - (cdr_lens[i]+4):const_idxs[i]] for i in range(len(full_seqs))]
+
+    def hamming_dist(s1, s2):
+        assert len(s1) == len(s2)
+        hd = 0
+        for b1, b2 in zip(s1, s2):
+            if b1 != b2:
+                hd += 1
+        return hd
+
+    total_hd = 0
+    num_pairs = 0
+    
+    for i in range(len(gen_seqs)):
+        seq1 = gen_seqs[i]
+        for j in range(i+1, len(gen_seqs)):
+            seq2 = gen_seqs[j]
+            total_hd += hamming_dist(seq1, seq2)
+            num_pairs += 1
+
+    return total_hd/num_pairs
+        
+
 if __name__ == "__main__":
 
     calc_unconditiona_kl('results/trbv_gen.csv')
