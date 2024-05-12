4b41dba4c61c9f7943ef0428f075f8b1d41d9973
Rachit Mukkamala <rsmukk@mit.edu>
2024-05-12 16:23:53 -0400

    finished sampler starter code

--- src/main_sampler.py
+++ src/main_sampler.py
@@ -2,57 +2,79 @@ import csv
 import pandas as pd
 import subprocess
 from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import random
 
-def process_V_regions(csv_file):
-    
+def process_scaffolds(csv_file):
     seq_table = pd.read_csv(csv_file)
-    left_scaffolds = {}
+    scaffolds = []
+    scaffold_ids = []
     for row in range(seq_table.shape[0]):
         id = seq_table.iloc[row, 0]
         sequence = ''.join(seq_table.iloc[row, 1])
-        scaffold_left = sequence.replace(".", "")
-        left_scaffolds[id] = scaffold_left
-            
-    return left_scaffolds
+        scaffolds.append(sequence.replace(".", ""))
+        scaffold_ids.append(id)
+    return pd.Series(scaffolds, index=scaffold_ids)
 
-def process_J_regions(csv_file):
-    
-    seq_table = pd.read_csv(csv_file)
-    mid_scaffolds = {}
-    for row in range(seq_table.shape[0]):
-        id = seq_table.iloc[row, 0]
-        sequence = ''.join(seq_table.iloc[row, 1])
-        scaffold_mid = sequence.replace(".", "")
-        mid_scaffolds[id] = scaffold_mid
-            
-    return mid_scaffolds
 
 def gen_uniform_VJ_table(V_list, J_list):
-    f
+    freq = 1/(len(V_list) * len(J_list))
+    VJ_ids = [f'{V};{J}' for V in V_list for J in J_list]
+    return pd.Series([freq for i in range(len(VJ_ids))], index=VJ_ids)
+
+
+def gen_uniform_CDR3_table(CDR3_lengths):
+    freq = 1/len(CDR3_lengths)
+    return pd.Series([freq for i in range(len(CDR3_lengths))], index=CDR3_lengths)
+
+def sample_VJ(k, V_scaffolds, J_scaffolds, VJ_table):
+    for choice in random.choices(k=k, population=VJ_table.index, weights=VJ_table.values):
+        V_id, J_id = choice.split(';')
+        print(V_id, J_id)
+        yield V_scaffolds[V_id], J_scaffolds[J_id]
+
+def sample_CDR3(k, CDR3_table):
+    for choice in random.choices(k=k, population=CDR3_table.index, weights=CDR3_table.values):
+        yield choice
 
 if __name__ == "__main__":
-    V_region_file = "data/TRJ_human_imgt.csv"
-    J_region_file = "data/TRBV_human_imgt.csv"
+    chain = 'B'
+    V_region_file = f"data/TR{chain}V_human_imgt.csv"
+    J_region_file = f"data/TR{chain}J_human_imgt.csv"
 
-    left_scaffolds = process_V_regions(V_region_file)
-    mid_scaffolds = process_J_regions(J_region_file)
-    with open('data/trbc2_seq.txt', 'r') as file:
-        right_scaffold = file.read().strip()
+    V_scaffolds = process_scaffolds(V_region_file)
+    J_scaffolds = process_scaffolds(J_region_file)
+    with open(f'data/TR{chain}C_human_imgt.txt', 'r') as file:
+        C_scaffold = file.read().strip()
 
-    num_samples = 10000
+    VJ_table = gen_uniform_VJ_table(V_scaffolds.index, J_scaffolds.index)
+    CDR3_table = gen_uniform_CDR3_table(np.arange(8, 22))
+    
+    num_samples = 3
     model_type = "oa_dm_38M"
+    VJ_gen = sample_VJ(num_samples, V_scaffolds, J_scaffolds, VJ_table)
+    CDR3_gen = sample_CDR3(num_samples, CDR3_table)
+
 
     for i in range(num_samples):
-        for cdr3_len in tqdm(range(8,12)): # first 3 residues already in prompt
-            for j in range(num_repeats):
-                command = [
-                    "python",
-                    "evodiff/evodiff/conditional_generation.py",
-                    f"--model-type={model_type}",
-                    "--gpus=6",
-                    "--cond-task=tcr",
-                    f"--scaffold_chain_left={left_scaffolds[id]}",
-                    f"--scaffold_chain_right={right_scaffold}",
-                    f"--cdr3_len={cdr3_len}"
-                ]
-                subprocess.run(command)
+        V_scaffold, J_scaffold = next(VJ_gen)
+        CDR3_len = next(CDR3_gen)
+        left_scaffold = V_scaffold
+        right_scaffold = J_scaffold + C_scaffold
+        print(V_scaffold)
+        print(J_scaffold)
+        print(CDR3_len)
+        print()
+
+        command = [
+            "python",
+            "evodiff/evodiff/conditional_generation.py",
+            f"--model-type={model_type}",
+            "--gpus=6",
+            "--cond-task=tcr",
+            f"--scaffold_chain_left={left_scaffold}",
+            f"--scaffold_chain_right={right_scaffold}",
+            f"--cdr3_len={CDR3_len}"
+        ]
+        subprocess.run(command)
