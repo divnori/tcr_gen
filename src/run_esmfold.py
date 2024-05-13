1697614d7bf1a5c0844ccd92db5bf125c729ecb5
Divya Nori <divnor80@mit.edu>
2024-05-12 18:27:42 -0700

    folding script update

--- src/run_esmfold.py
+++ src/run_esmfold.py
@@ -3,37 +3,44 @@ import csv
 import esm
 import os
 import torch
+from tqdm import tqdm
 
 model = esm.pretrained.esmfold_v1()
 model = model.eval().cuda()
 
 # Path to the input CSV file
-csv_file = 'results/trbv_gen_example.csv'
+csv_file = 'results/trbv_gen.csv'
 
 # Path to the results folder
 results_folder = 'results/structures'
+
+results_csv_path = 'results/plddt_results.csv'
 
 with open(csv_file, 'r') as file:
     reader = csv.reader(file)
     sequences = [row[0] for row in reader]
 
-plddts = []
-for i, sequence in enumerate(sequences):
+with open(results_csv_path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Sequence', 'pLDDT'])  
     
-    with torch.no_grad():
-        output = model.infer_pdb(sequence)
+    for i, sequence in tqdm(enumerate(sequences)):
 
-    with open(os.path.join(results_folder, f"prot{i}.pdb"), "w") as f:
-        f.write(output)
+        try:
+        
+            with torch.no_grad():
+                output = model.infer_pdb(sequence)
 
-    struct = bsio.load_structure("result.pdb", extra_fields=["b_factor"])
-    plddt = struct.b_factor.mean()
-    plddts.append(plddt)
+            with open(os.path.join(results_folder, f"prot{i}.pdb"), "w") as f:
+                f.write(output)
 
-output_file = 'results/plddts.csv'
-with open(output_file, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Sequence', 'PLDDT'])
-    for sequence, plddt in zip(sequences, plddts):
-        writer.writerow([sequence, plddt])
-    
+            struct = bsio.load_structure(os.path.join(results_folder, f"prot{i}.pdb"), extra_fields=["b_factor"])
+            plddt = struct.b_factor.mean()
+            
+            writer.writerow([sequence, plddt])
+
+        except Exception as e:
+
+            print(e)
+
+            print("error on sequence", i)
